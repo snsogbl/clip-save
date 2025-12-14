77aebd2f01c1bfd9e1357315c925177186cf6c44
snsogbl <snsogbl1@163.com>
2025-12-14 20:12:30 +0800

    Add JSDoc comments for deduplicateText function
    Added JSDoc comments to describe the function's purpose and parameters.

--- scriptingExample/deduplicateText.js
+++ scriptingExample/deduplicateText.js
@@ -1,8 +1,10 @@
-// 示例脚本JavaScript代码
-// item.Content - 剪贴板内容
-// item.ContentType - 内容类型 (Text, Image, File, URL, Color, JSON)
-// item.Timestamp - 时间戳
-// item.Source - 来源应用
+/**
+ * 统计文本中每行内容的出现次数
+ * @author github.com/qyuja
+ * @param {string} input - 输入的字符串
+ * @returns {string} - 返回重复出现次数 eg: hello world (2) foo bar (3)
+ * @returns {object} - 错误信息
+ */
 
 
 if (item.ContentType !== "Text") {
