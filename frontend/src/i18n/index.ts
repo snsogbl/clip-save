d3b970b1f34f39d155380c32140dde16f3014373
yangjie <snsogbl1@163.com>
2025-12-22 09:55:22 +0800

    OCR 识别

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -1,7 +1,7 @@
 import { createI18n } from 'vue-i18n'
 import { GetCurrentLanguage } from '../../wailsjs/go/main/App'
 
-const AppVersion = '2.2.5'
+const AppVersion = '2.2.6'
 
 // 中文语言包
 const zhCN = {
